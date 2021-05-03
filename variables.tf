dfa385044aaf3ba088a7bbcd8bad573eb037f132
Tim Gibbon <tng@chegwin.org>
2021-05-03 12:57:03 +0100

    Moved EC2 to public network, commented and variiables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
 # Variable declarations
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_count" {
+  description = "Number of instances to provision."
+  type        = number
+  default     = 1
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
